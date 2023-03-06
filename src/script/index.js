8b0457310cac3a09fe62b4350754187f9115b096
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-03-07 01:09:08 +0530

    adding image

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,5 @@
 function bodyload(){
-    window.document.image[0].src="public/images/tree1.jpg";
+    window.document.images[0].src="public/images/tree1.jpg";
+    window.document.images[0].width=200;
+    
 }
