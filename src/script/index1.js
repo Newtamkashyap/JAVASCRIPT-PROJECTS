13314ba8c19e7175f962defb7526c628a312d032
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-03-07 22:00:51 +0530

    adding javascript techniques

--- src/script/index1.js
+++ src/script/index1.js
@@ -1,3 +1,16 @@
 function Deleteclick(){
-    alert("delete \n record will be deleted");
+    flag= confirm("delete \n record will be deleted");
+    if (flag ==true){
+       // document.write("<b><i><font color=red> record delete</font></i></b><br><a href=index1.html>back</a>")
+       //document.querySelector("p").innerText = "record deleted successfully";
+       //document.querySelector("h3").innerText = "record deleted confirmed"
+       //document.querySelector("input[type=text]").placeholder="record deleted"
+      // document.querySelector("p").innerHTML="<font color= red>RECORD DELETED</font>";
+      document.querySelector("p").outerHTML="<font color= red>RECORD DELETED</font>";
+      
+    }
+    else{
+        alert("you canceled")
+    }
+   
 }
